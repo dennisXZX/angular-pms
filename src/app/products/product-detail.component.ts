4dafd221852a7bafc2e2109f1d7053929250689b
Dennis Xiao <dennisboys@gmail.com>
2017-11-29 21:29:30 +1100

    added product detail view

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,15 +10,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
+  errorMessage: string;
   product: IProduct;
 
   constructor(private _route: ActivatedRoute,
-              private _router: Router) { }
+              private _router: Router,
+              private _productService: ProductService) { }
 
   ngOnInit() {
     const id = parseInt(this._route.snapshot.paramMap.get('id'), 10);
-    this.pageTitle += `: ${id}`;
+    this.getProduct(id);
+  }
 
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
 
   onBack(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -15,12 +16,28 @@ export class ProductService {
   getProducts(): Observable<IProduct[]> {
     return this._http.get<IProduct[]>(this._productURL)
                // use do method to help with debug logging
-               .do(data => console.log(JSON.stringify(data)))
+               // .do(data => console.log(JSON.stringify(data)))
                .catch(this.handleError);
   }
 
+  getProduct(id: number): Observable<IProduct> {
+    return this.getProducts()
+      .map((products: IProduct[]) => products.find(p => p.productId === id));
+  }
+
   private handleError(err: HttpErrorResponse) {
-    console.log(err.message);
-    return Observable.throw(err.message);
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof Error) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.log(errorMessage);
+    return Observable.throw(errorMessage);
   }
 }
